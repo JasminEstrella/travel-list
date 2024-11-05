766c4459cba67a8bd2df901f62c2984107c0dbd4
Jaja <jestrella@pacificseainvests.com>
2024-11-05 11:44:00 +0800

    add clear list function

--- src/App.js
+++ src/App.js
@@ -23,12 +23,17 @@ function App() {
       {...item, packed: !item.packed} : item))
   }
 
+  function handleClearList() {
+    const confirmed = window.confirm("Are you sure you want to delete all items?");
+
+    if (confirmed) setItems([]);
+  }
 
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem}/>
+      <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} onClearList={handleClearList}/>
       <Stats items={items} />
     </div>
 
@@ -69,7 +74,7 @@ function Form({onAddItems}) {
   )
 }
 
-function PackingList({items, onDeleteItem, onToggleItem}) {
+function PackingList({items, onDeleteItem, onToggleItem, onClearList}) {
   const [sortBy, setSortBy] = useState('input');
   let sortedItems;
 
@@ -97,6 +102,8 @@ function PackingList({items, onDeleteItem, onToggleItem}) {
           <option value='description'>Sort by description</option>
           <option value='packed'>Sort by packed status</option>
         </select>
+        { items.length > 0 &&
+          <button onClick={onClearList}>Clear list</button>}
       </div>
     </div>
   )
